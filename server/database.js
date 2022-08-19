c81ffda8bc523ede7bb909d01cc7b012aec6f069
djcoleman1 <djcoleman@umass.edu>
2022-08-19 13:16:49 -0400

    Added/initiated Express from the terminal, added node_modules/ to .gitignore

---
+++ server/database.js
@@ -0,0 +1,13 @@
+import { readFile, writeFile } from 'fs/promises';
+
+// A class representing a database to keep scores
+class Database {
+  constructor() {
+    
+  }
+
+}
+
+const database = new Database();
+
+export { database };
---
+++ server/index.js
@@ -0,0 +1,20 @@
+import { database } from './database.js';
+
+import express from 'express';
+import logger from 'morgan';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use('/', express.static('client'));
+
+app.get();
+
+app.put();
+
+app.post();
+
+app.delete();
